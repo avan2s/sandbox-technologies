c331cc94b36e15ba12b78c50d55289df66a79daa
Andreas van Helden <andreas-van-helden@gmx.de>
2023-09-27 15:04:56 +0200

    add producer

--- kafka/kafka-app/topic.ts
+++ kafka/kafka-app/topic.ts
@@ -5,7 +5,7 @@ const topics: ITopicConfig[] = [{ topic: 'Users', numPartitions: 2 }];
 const run = async () => {
   const kafka = new Kafka({
     clientId: 'my-app',
-    brokers: ['kafka:9092'], // Use the service name defined in docker-compose.yml
+    brokers: ['kafka:9092'],
   });
 
   const admin = kafka.admin();
