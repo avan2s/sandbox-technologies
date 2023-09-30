5ad5a8f1e3b0a6c39468dec6a1571a91cb091daf
Andreas van Helden <andreas-van-helden@gmx.de>
2023-09-30 11:04:22 +0200

    harden the topic creator not to fail if topic already exist

--- kafka/kafka-app/topic.ts
+++ kafka/kafka-app/topic.ts
@@ -1,7 +1,7 @@
 import { ITopicConfig, Kafka } from "kafkajs";
 
-const TOPICS_TO_CREATE = (process.env.TOPICS_TO_CREATE || 'users').split(',').map(s => s.trim());
-const topics: ITopicConfig[] = TOPICS_TO_CREATE.map(t => {return { topic: t }});
+const TOPICS_TO_MAINTAIN: ITopicConfig[] = (process.env.TOPICS_TO_CREATE || 'users').split(',').map(s => s.trim())
+.map(t => {return { topic: t, numPartitions: 2 }});
 // default will be container network name, otherwise the argument which you can provide
 const KAFKA_HOST = process.argv[2] || 'kafka';
 
@@ -15,10 +15,17 @@ const run = async () => {
   console.log('Connecting...');
   await admin.connect();
   console.log('Connected');
-  await admin.createTopics({
-    topics: topics,
-  });
-  console.log(`Created topics: ${topics.map(t => t.topic).join(',')}`);
+  const existingTopics = await admin.listTopics();
+  const topicsToCreate: ITopicConfig[] = TOPICS_TO_MAINTAIN.filter(t => !existingTopics.includes(t.topic));
+
+  if(topicsToCreate.length > 0) {
+    await admin.createTopics({
+      topics: topicsToCreate,
+    });
+    console.log(`Created topics: ${topicsToCreate.map(t => t.topic).join(',')}`);
+  } else {
+    console.log(`topics already exist`)
+  }
   await admin.disconnect();
   console.log('Disconnected');
 };
